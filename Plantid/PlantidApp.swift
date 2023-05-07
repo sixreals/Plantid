8bc29a8da130b18d5b497d77bce3edfd5759e8ab
sixreals <ry_walt@posteo.net>
2023-05-07 16:13:54 +0800

    Added env commands to MainMenu

--- Plantid/PlantidApp.swift
+++ Plantid/PlantidApp.swift
@@ -13,8 +13,7 @@ struct PlantidApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainMenuView().environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
